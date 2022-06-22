90da5d4189363f1caae208305c31d69c7b5c4a96
구정연 <jykoo@gujeong-yeon-ui-MacBookPro.local>
2022-06-22 21:12:35 +0900

    환경세팅

---
+++ src/main/java/com/plent/plantrow/PlantrowApplication.java
@@ -0,0 +1,13 @@
+package com.plent.plantrow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlantrowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlantrowApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/plent/plantrow/PlantrowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.plent.plantrow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlantrowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
